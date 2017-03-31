45af7ee28e527cc374cd76806c7f7006108f96c3
Orlov Kirill <orlovkirill31@mail.ru>
2017-03-31 20:18:23 +0300

    Null

---
+++ LIFE 2.1 [BlackWar]/LIFE/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace LIFE
+    {
+    
+    public partial class Form1: Form
+        {
+        public static bool[,] staticTerrain;
+        public static bool IsRandom = true;
+
+        public static int size { get; private set; }
+        public static int sizeOfWindow { get { return 600; } }
+        public static int sizeOfCell { get; }
+
+        ScannerTerrainDecorator scanTerDec;
+        StatisticsTerrainDecorator statTerDec;
+        FramedTerrainDecorator framTerDec;
+        Terrain terrain;
+        Form2 f2;
+        
+
+        public Form1 ()
+            {
+            InitializeComponent();
+            }
+
+        private void SIZE_Scroll (object sender , EventArgs e)
+            {
+            label_sizeValue.Text = trackBar_SIZE.Value.ToString();
+            trackBar_FOUNDERS.Maximum = Convert.ToInt32(Math.Pow(trackBar_SIZE.Value , 2));
+            label_foundersValue.Text = trackBar_FOUNDERS.Value.ToString();
+            }
+
+        private void FOUNDERS_Scroll (object sender , EventArgs e)
+            {
+            label_foundersValue.Text = trackBar_FOUNDERS.Value.ToString();
+            }
+        private void MANUALLY_Click (object sender , EventArgs e)
+            {
+            size = trackBar_SIZE.Value;
+            f2 = new Form2(size);
+            f2.Owner = this;
+            f2.Show();
+            checkBox1.Visible = false;
+            }
+        private void RAND_Click (object sender , EventArgs e)
+            {
+            IsRandom = true;
+            Show_GO();
+            checkBox1.Visible = false;
+            }
+
+        public void Show_GO ()
+            {
+            MANUALLY.Visible = RAND.Visible = false;
+            label_foundersValue.Visible = trackBar_FOUNDERS.Visible = true;
+            GO.Visible = REPLAY.Visible = true;
+            if (!IsRandom)
+               GO.PerformClick();
+            }
+
+        public void GO_Click (object sender , EventArgs e)
+            {
+            trackBar_SIZE.Enabled = trackBar_FOUNDERS.Enabled =  GO.Visible = false;
+            PLAY.Visible = NEXT_STEP.Visible = label_stepsValue.Visible = label_steps.Visible = label_delayValue.Visible = label_delay.Visible = STEPS.Visible = DELAY.Visible = true;
+            size = trackBar_SIZE.Value;
+            
+            Start(size);
+
+            if (IsRandom)
+                Random(trackBar_FOUNDERS.Value);
+            else
+                Array.Copy(f2.ManuallyTerrain , staticTerrain , size * size);
+
+            terrain = new Terrain(this);
+            scanTerDec = new ScannerTerrainDecorator(this);
+            //statTerDec = new StatisticsTerrainDecorator(this);
+            //framTerDec = new FramedTerrainDecorator(this);
+
+            scanTerDec.SetTerrain(terrain);
+            //statTerDec.SetTerrain(scanTerDec.terrain);
+
+            //if (!checkBox1.Checked)
+            //    {
+            //    framTerDec.SetTerrain(statTerDec.terrain);
+            //    framTerDec.Draw(this.pictureBox1);
+            //    }
+            //else
+            //    {
+            //    statTerDec.Draw(pictureBox1);
+            //    }
+            
+            scanTerDec.BlackDraw(pictureBox1);
+            this.Update();
+            
+            }
+
+        private void Random (int founders)
+            {
+            Random rand = new Random();
+            for (int i = 0 ; i < founders ; i++)
+                {
+                int x = rand.Next(0 , size);
+                int y = rand.Next(0 , size);
+                if (!staticTerrain[x , y])
+                    {
+                    staticTerrain[x , y] = true;
+                    }
+                else
+                    {
+                    i--;
+                    //continue;
+                    }
+                }
+            }
+
+        private void Start (int size)
+            {
+            MANUALLY.Enabled = RAND.Enabled = false;
+            REPLAY.Visible = true;
+            staticTerrain = new bool[size , size];
+            
+            
+            }
+
+        private void REPLAY_Click (object sender , EventArgs e)
+            {
+            pictureBox1.Image = null;
+            pictureBox2.Image = null;
+            //checkBox1.Visible = true;
+            MANUALLY.Visible = RAND.Visible = MANUALLY.Enabled = RAND.Enabled = trackBar_SIZE.Enabled = trackBar_FOUNDERS.Enabled = GO.Visible = true;
+            REPLAY.Visible = GO.Visible = label_foundersValue.Visible = trackBar_FOUNDERS.Visible = PLAY.Visible = NEXT_STEP.Visible = label_stepsValue.Visible = label_steps.Visible = label_delayValue.Visible = label_delay.Visible = STEPS.Visible = DELAY.Visible = false;
+            //turnf = null;
+            //step = null;
+            }
+
+        private void STEPS_Scroll (object sender , EventArgs e) 
+            {
+            label_stepsValue.Text = STEPS.Value.ToString();
+            }
+
+        private void NEXT_STEP_Click (object sender , EventArgs e)
+            {
+            
+            scanTerDec.MakeTurn();
+            this.Update();
+            //scanTerDec.AfterInBefore();
+            //scanTerDec.AfterInFalse();
+            }
+        
+        private void DELAY_Scroll (object sender , EventArgs e)
+            {
+            label_delayValue.Text = DELAY.Value.ToString();
+            }
+
+        private void PLAY_Click (object sender , EventArgs e)
+            {
+            for (int i = 0 ; i < STEPS.Value ; i++)
+                {
+                NEXT_STEP.PerformClick();
+                pictureBox1.Update();
+                Thread.Sleep(DELAY.Value);
+                }
+            }
+
+        private void NEXT_STEP_KeyDown (object sender , KeyEventArgs e)
+            {
+            NEXT_STEP.PerformClick();
+            }
+        
+        }
+    }
---
+++ LIFE 2.1 [BlackWar]/LIFE/Form2.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace LIFE
+    {
+    partial class Form2
+        {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose (bool disposing)
+            {
+            if (disposing && (components != null))
+                {
+                components.Dispose();
+                }
+            base.Dispose(disposing);
+            }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent ()
+            {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.CANCEL = new System.Windows.Forms.Button();
+            this.APPLY = new System.Windows.Forms.Button();
+            this.CLEAN = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(5, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1070, 1070);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // CANCEL
+            // 
+            this.CANCEL.FlatAppearance.BorderSize = 3;
+            this.CANCEL.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CANCEL.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CANCEL.ForeColor = System.Drawing.Color.Maroon;
+            this.CANCEL.Location = new System.Drawing.Point(1081, 792);
+            this.CANCEL.Margin = new System.Windows.Forms.Padding(0);
+            this.CANCEL.Name = "CANCEL";
+            this.CANCEL.Size = new System.Drawing.Size(216, 283);
+            this.CANCEL.TabIndex = 1;
+            this.CANCEL.Text = "CANCEL";
+            this.CANCEL.UseVisualStyleBackColor = true;
+            this.CANCEL.Click += new System.EventHandler(this.CANCEL_Click);
+            // 
+            // APPLY
+            // 
+            this.APPLY.FlatAppearance.BorderSize = 5;
+            this.APPLY.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.APPLY.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.APPLY.ForeColor = System.Drawing.Color.Green;
+            this.APPLY.Location = new System.Drawing.Point(1081, 306);
+            this.APPLY.Name = "APPLY";
+            this.APPLY.Size = new System.Drawing.Size(216, 480);
+            this.APPLY.TabIndex = 2;
+            this.APPLY.Text = "APPLY";
+            this.APPLY.UseVisualStyleBackColor = true;
+            this.APPLY.Click += new System.EventHandler(this.APPLY_Click);
+            // 
+            // CLEAN
+            // 
+            this.CLEAN.FlatAppearance.BorderSize = 3;
+            this.CLEAN.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CLEAN.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CLEAN.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.CLEAN.Location = new System.Drawing.Point(1081, 5);
+            this.CLEAN.Name = "CLEAN";
+            this.CLEAN.Size = new System.Drawing.Size(216, 295);
+            this.CLEAN.TabIndex = 3;
+            this.CLEAN.Text = "CLEAN";
+            this.CLEAN.UseVisualStyleBackColor = true;
+            this.CLEAN.Click += new System.EventHandler(this.CLEAN_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1300, 1080);
+            this.Controls.Add(this.CLEAN);
+            this.Controls.Add(this.APPLY);
+            this.Controls.Add(this.CANCEL);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form2";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+            }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button CANCEL;
+        private System.Windows.Forms.Button APPLY;
+        private System.Windows.Forms.Button CLEAN;
+        }
+    }
